57ca3cfdbc8b2cc760554a45074b4ed90bf5019f
CrazyPants <crazypants.mc@gmail.com>
2014-11-15 23:39:29 +0800

    Updated release notes.

--- src/main/java/crazypants/enderzoo/entity/EntityDireWolf.java
+++ src/main/java/crazypants/enderzoo/entity/EntityDireWolf.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILeapAtTarget;
@@ -84,20 +83,11 @@ public class EntityDireWolf extends EntityMob implements IEnderZooMob {
     return 6;
   }
 
-  /**
-   * Returns true if the entity is of the @link{EnumCreatureType} provided
-   * 
-   * @param type
-   *          The EnumCreatureType type this entity is evaluating
-   * @param forSpawnCount
-   *          If this is being invoked to check spawn count caps.
-   * @return If the creature is of the type provided
-   */
-  @Override
-  public boolean isCreatureType(EnumCreatureType type, boolean forSpawnCount) {
-    System.out.println("EntityDireWolf.isCreatureType: " + type);
-    return type.getCreatureClass().isAssignableFrom(this.getClass());
-  }
+  //  @Override
+  //  public boolean isCreatureType(EnumCreatureType type, boolean forSpawnCount) {
+  //    System.out.println("EntityDireWolf.isCreatureType: " + type);
+  //    return type.getCreatureClass().isAssignableFrom(this.getClass());
+  //  }
 
   private void updateAngry() {
     getDataWatcher().updateObject(ANGRY_INDEX, getAttackTarget() != null ? (byte) 1 : (byte) 0);
