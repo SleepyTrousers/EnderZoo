658564e08d3b60ea685c9ffb6771c0fe45278b8d
tterrag1098 <tterrag1098@gmail.com>
2015-04-11 03:09:21 -0400

    Remove debug code...

--- src/main/java/crazypants/enderzoo/entity/render/RenderWitherCat.java
+++ src/main/java/crazypants/enderzoo/entity/render/RenderWitherCat.java
@@ -31,8 +31,9 @@ public class RenderWitherCat extends RenderLiving {
   @Override
   public void doRender(EntityLiving entity, double x, double y, double z, float p_76986_8_, float p_76986_9_) {
     super.doRender(entity, x, y, z, p_76986_8_, p_76986_9_);
-    GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
-    RenderUtil.renderEntityBoundingBox(entity, x, y, z);
+    // CrazyPants pls
+//    GL11.glDisable(GL11.GL_POLYGON_OFFSET_FILL);
+//    RenderUtil.renderEntityBoundingBox(entity, x, y, z);
   }
 
 
